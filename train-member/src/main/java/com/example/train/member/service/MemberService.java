db4e502a9db1c0b27effdecb20822d0596f4865e
luf <javnotes@gmail.com>
2023-03-17 11:38:46 +0800

    集成Mybatis官方代码生成器，成功访问数据库

--- train-member/src/main/java/com/example/train/member/service/MemberService.java
+++ train-member/src/main/java/com/example/train/member/service/MemberService.java
@@ -14,6 +14,6 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
     public int count() {
-        return memberMapper.count();
+        return (int) memberMapper.countByExample(null);
     }
 }
